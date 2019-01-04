a1db03cc8bc2d701e2c983b54d73c2ad6d506f25
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-04 06:19:24 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,7 +9,10 @@ class CouponsController < ApplicationController
 
   def create
     # binding.pry
-    coupon = Coupon.create!(params[:coupon])
+    coupon = Coupon.new
+    coupon.coupon_code = params[:coupon][:coupon_code]
+    coupon[:store] = params[:coupon][:store]
+    coupon.save
     redirect_to coupon_path(coupon)
   end
 
